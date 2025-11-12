80fefc5e9e94a48d5b1662c018178ee436d39dae
Carlos Soares <domcarlito@gmail.com>
2025-11-12 15:30:04 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "./components/Header";
 
 export const metadata = {
   title: "Classilagos",
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-white text-slate-900">{children}</body>
+      <body className="bg-white text-slate-900">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
+
