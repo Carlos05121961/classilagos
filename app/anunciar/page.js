1d3ceb3935b99c44daa66ea4c503d3d9514b3152
Carlos Soares <domcarlito@gmail.com>
2025-11-25 01:06:07 -0300

    Update page.js

--- app/anunciar/page.js
+++ app/anunciar/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import FormularioImoveis from "../../components/forms/FormularioImoveis";
++import FormularioImoveis from "../components/forms/FormularioImoveis";
 
 export default function AnunciarImovelPage() {
   return (
