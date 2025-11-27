197a3d1c587ed221ae6e8ed526d4a4e416150a84
Carlos Soares <domcarlito@gmail.com>
2025-11-27 13:58:35 -0300

    Update page.js

--- app/servicos/page.js
+++ app/servicos/page.js
@@ -19,7 +19,8 @@ export default function ServicosPage() {
         .select(
           "id, titulo, cidade, bairro, faixa_preco, atende_domicilio, subcategoria_servico, imagens"
         )
-        .eq("categoria", "servicos")
+       .or("categoria.eq.servicos,categoria.eq.servico,categoria.eq.serviços")
+,
         .eq("status", "ativo")
         .order("created_at", { ascending: false });
 
