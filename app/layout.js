edce8ce217b9409b2cf6c610ebad4cad595308c0
Carlos Soares <domcarlito@gmail.com>
2025-11-29 23:22:10 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import SiteHeader from "./components/SiteHeader";
 import AuthProvider from "./AuthProvider";
+import MobileAnunciarBar from "./components/MobileAnunciarBar";
 
 export const metadata = {
   title: "Classilagos",
@@ -10,10 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-white text-slate-900">
+      {/* pb-12 garante espaço p/ barra fixa no mobile */}
+      <body className="bg-white text-slate-900 pb-14 md:pb-0">
         <AuthProvider>
           <SiteHeader />
           {children}
+          {/* Barra fixa no mobile */}
+          <MobileAnunciarBar />
         </AuthProvider>
       </body>
     </html>
