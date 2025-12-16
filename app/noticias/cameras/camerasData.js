68f8d4f086f04d99e2dd3d216fcfa725a9d37cc9
Carlos Soares <domcarlito@gmail.com>
2025-12-16 18:14:30 -0300

    Create camerasData.js

---
+++ app/noticias/cameras/camerasData.js
@@ -0,0 +1,76 @@
+export const camerasData = [
+  {
+    grupo: "Rodovias",
+    itens: [
+      {
+        titulo: "Via Lagos – RJ-124",
+        local: "Região dos Lagos",
+        descricao: "Condições de tráfego na Via Lagos",
+        url: "https://www.vialagos.com.br/",
+        embed: false,
+      },
+      {
+        titulo: "RJ-106",
+        local: "Litoral Leste Fluminense",
+        descricao: "Rodovia Amaral Peixoto",
+        url: "https://www.der.rj.gov.br/",
+        embed: false,
+      },
+    ],
+  },
+  {
+    grupo: "Ponte / Trânsito",
+    itens: [
+      {
+        titulo: "Ponte Rio–Niterói",
+        local: "Rio de Janeiro",
+        descricao: "Fluxo de veículos em tempo real",
+        url: "https://www.ecoponte.com.br/",
+        embed: false,
+      },
+    ],
+  },
+  {
+    grupo: "Praias",
+    itens: [
+      {
+        titulo: "Praia de Itaúna",
+        local: "Saquarema",
+        descricao: "Condições do mar e da praia",
+        url: "https://www.youtube.com/results?search_query=praia+itauna+ao+vivo",
+        embed: false,
+      },
+      {
+        titulo: "Praia do Forte",
+        local: "Cabo Frio",
+        descricao: "Movimento da orla",
+        url: "https://www.youtube.com/results?search_query=praia+do+forte+cabo+frio+ao+vivo",
+        embed: false,
+      },
+    ],
+  },
+  {
+    grupo: "Centros / Orlas",
+    itens: [
+      {
+        titulo: "Centro de Cabo Frio",
+        local: "Cabo Frio",
+        descricao: "Movimento urbano",
+        url: "https://www.youtube.com/results?search_query=centro+cabo+frio+ao+vivo",
+        embed: false,
+      },
+    ],
+  },
+  {
+    grupo: "Tempo / Mar (YouTube)",
+    itens: [
+      {
+        titulo: "Canal Surf Report Região dos Lagos",
+        local: "Região dos Lagos",
+        descricao: "Ondas, vento e condições do mar",
+        url: "https://www.youtube.com/",
+        embed: false,
+      },
+    ],
+  },
+];
