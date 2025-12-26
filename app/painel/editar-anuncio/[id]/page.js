bffcee5b4aa34d9ccfd14af853dbdfe0cb7b9b68
Carlos Soares <domcarlito@gmail.com>
2025-12-26 19:26:53 -0300

    Update page.js

--- app/painel/editar-anuncio/[id]/page.js
+++ app/painel/editar-anuncio/[id]/page.js
@@ -373,6 +373,7 @@ export default function EditarAnuncioPage() {
       payload.subcategoria_turismo = emptyToNull(form.subcategoria_turismo);
       payload.faixa_preco = emptyToNull(form.faixa_preco);
       payload.horario_atendimento = emptyToNull(form.horario_atendimento);
+      payload.horarioFuncionamento = emptyToNull(form.horario_atendimento);
       payload.nome_negocio = emptyToNull(form.nome_negocio);
       payload.site_url = emptyToNull(form.site_url);
       payload.instagram = emptyToNull(form.instagram);
