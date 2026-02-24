c90e04c861aede551fd3c9ba3e7aa33af5478ba6
Carlos Soares <domcarlito@gmail.com>
2026-02-24 10:01:51 -0300

    Update layout.js
    Instalação Google Tag Manager GTM-N9MPL3QF

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import SiteHeader from "./components/SiteHeader";
 import SiteFooter from "./components/SiteFooter";
 import Script from "next/script";
@@ -18,16 +19,37 @@ export default function RootLayout({ children }) {
           strategy="afterInteractive"
         />
         <Script id="google-ads-gtag" strategy="afterInteractive">
-          {`
+         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-17865509628');
           `}
         </Script>
+          <Script
+  id="gtm"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N9MPL3QF');
+    `,
+  }}
+/>
       </head>
 
       <body className="bg-slate-950 text-slate-50 overflow-x-hidden">
+  <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-N9MPL3QF"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
         <SiteHeader />
         {children}
         <SiteFooter />
