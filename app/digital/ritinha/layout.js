16c4c9b8bdd442e25f1fe36596bb46edb804270d
Carlos Soares <domcarlito@gmail.com>
2026-03-31 08:45:19 -0300

    Update layout.js

--- app/digital/ritinha/layout.js
+++ app/digital/ritinha/layout.js
@@ -1,3 +1,20 @@
 export default function RitinhaLayout({ children }) {
-  return children;
+  return (
+    <div style={{ background: "#ffffff" }}>
+      {children}
+
+      {/* Assinatura discreta */}
+      <div
+        style={{
+          textAlign: "center",
+          fontSize: "12px",
+          color: "#999",
+          marginTop: "40px",
+          padding: "20px 0",
+        }}
+      >
+        Produzido por Classilagos.shop
+      </div>
+    </div>
+  );
 }
