3b7401d1ab182286b05c70b938e83b98d9481b64
Carlos Soares <domcarlito@gmail.com>
2026-04-10 13:39:20 -0300

    Create layout.js

---
+++ app/digital/carlinhos-gas/layout.js
@@ -0,0 +1,26 @@
+export const metadata = {
+  title: "Carlinhos do Gás | Há 50 anos em Maricá",
+  description:
+    "Entrega rápida de gás em Maricá com segurança e confiança.",
+
+  openGraph: {
+    title: "Carlinhos do Gás | Há 50 anos em Maricá",
+    description:
+      "Entrega rápida de gás em Maricá com segurança e confiança.",
+    url: "https://classilagos.shop/digital/carlinhos-gas",
+    siteName: "Classilagos",
+    images: [
+      {
+        url: "https://classilagos.shop/digital/carlinhos-gas/opengraph-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
