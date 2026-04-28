6b6b2bd985258b6f0a0f519df185a0785a78a76d
Carlos Soares <domcarlito@gmail.com>
2026-04-28 11:50:34 -0300

    Update FormularioLagolistas.js

--- app/components/forms/FormularioLagolistas.js
+++ app/components/forms/FormularioLagolistas.js
@@ -335,10 +335,10 @@ export default function FormularioLagolistas() {
         return;
       }
 
-      // 🔥 CONVERSÃO GOOGLE ADS
+// 🔥 CONVERSÃO GOOGLE ADS - ENVIO DE ANÚNCIO
 if (typeof window !== "undefined" && window.gtag) {
-  window.gtag('event', 'conversion', {
-    'send_to': 'AW-17865509628/j9HeCPy0w50cEPyV-MZC'
+  window.gtag("event", "conversion", {
+    send_to: "AW-17865509628/7DbwCP6zhaQcEPyV-MZC",
   });
 }
 
