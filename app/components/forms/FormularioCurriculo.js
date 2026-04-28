a71a04dfaf2be97075dc3eab0051454e6fd6a2a1
Carlos Soares <domcarlito@gmail.com>
2026-04-28 11:44:12 -0300

    Update FormularioCurriculo.js

--- app/components/forms/FormularioCurriculo.js
+++ app/components/forms/FormularioCurriculo.js
@@ -392,10 +392,10 @@ export default function FormularioCurriculo() {
         return;
       }
 
-       // 🔥 CONVERSÃO GOOGLE ADS
+// 🔥 CONVERSÃO GOOGLE ADS - ENVIO DE ANÚNCIO
 if (typeof window !== "undefined" && window.gtag) {
-  window.gtag('event', 'conversion', {
-    'send_to': 'AW-17865509628/j9HeCPy0w50cEPyV-MZC'
+  window.gtag("event", "conversion", {
+    send_to: "AW-17865509628/7DbwCP6zhaQcEPyV-MZC",
   });
 }
 
