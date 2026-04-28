ddde82b3d663cd502f65cf56d6f91250deb251b7
Carlos Soares <domcarlito@gmail.com>
2026-04-28 11:58:09 -0300

    Update FormularioTurismo.js

--- app/components/forms/FormularioTurismo.js
+++ app/components/forms/FormularioTurismo.js
@@ -397,10 +397,10 @@ export default function FormularioTurismo() {
       return;
     }
 
-    // 🔥 CONVERSÃO GOOGLE ADS
+// 🔥 CONVERSÃO GOOGLE ADS - ENVIO DE ANÚNCIO
 if (typeof window !== "undefined" && window.gtag) {
-  window.gtag('event', 'conversion', {
-    'send_to': 'AW-17865509628/j9HeCPy0w50cEPyV-MZC'
+  window.gtag("event", "conversion", {
+    send_to: "AW-17865509628/7DbwCP6zhaQcEPyV-MZC",
   });
 }
 
